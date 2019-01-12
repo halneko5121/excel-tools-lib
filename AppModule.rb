95eb963e724b5bedd543b9387a3cc4111c896322
halneko <otsuichi5121@gmail.com>
2019-01-13 01:43:43 +0900

    comment: インデント揃え

--- AppModule.rb
+++ AppModule.rb
@@ -6,22 +6,22 @@ module AppModule
 	#----------------------------------------------
 	# アプリの main 関数
 	# @parm	title	アプリタイトル
-	# @parm	ver	アプリver
+	# @parm	ver		アプリver
 	# @parm	block	ブロック引数
 	#----------------------------------------------
 	def self.main( title, ver = nil, &block )
-	
+
 		title_param = nil
 		if( ver == nil )
 			title_param = "#{title}"
 		else
-			title_param = "#{title} Ver #{ver}"		
-		end	
+			title_param = "#{title} Ver #{ver}"
+		end
 		system("title #{title_param}")
 
 		puts "-----------------------------------------------------------"
 		puts "start\n\n"
-		
+
 		block.call()
 
 		puts "\nend"
