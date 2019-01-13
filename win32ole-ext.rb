0f5e661d544eb2844d28ddfa10e1109d69eaa100
halneko <otsuichi5121@gmail.com>
2019-01-13 16:54:59 +0900

    update: コード整理。コメント追記。

--- win32ole-ext.rb
+++ win32ole-ext.rb
@@ -1,18 +1,19 @@
 require 'win32ole'
 
 class WIN32OLE
-  @const_defined = Hash.new
-  
-  def WIN32OLE.new_with_const(prog_id, const_name_space)
+    @const_defined = Hash.new
 
-	result = WIN32OLE.new(prog_id)
+    # Excel オブジェクトの生成と定数の読み込み
+    def WIN32OLE.new_with_const(prog_id, const_name_space)
+        result = WIN32OLE.new(prog_id)
 
-    unless @const_defined[const_name_space] then
-		WIN32OLE.const_load(result, const_name_space)
-		@const_defined[const_name_space] = true
+        # 二重定義防止
+        unless @const_defined[const_name_space] then
+            # WIN32OLE定数の読み込み
+            # @note: WIN32OLE オブジェクトを生成しなければ定数を定義できない
+            WIN32OLE.const_load(result, const_name_space)
+            @const_defined[const_name_space] = true
+        end
+        return result
     end
-	
-    return result
-  end
-  
 end
